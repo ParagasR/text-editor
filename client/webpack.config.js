5617ba10e03baa801ef2ba2a0d404eede4a549be
ParagasR <87942857+ParagasR@users.noreply.github.com>
2022-01-28 08:37:01 -0700

    started the webpack plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,12 +19,20 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // plugins to add, here
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Jate',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      })
     ],
 
     module: {
       rules: [
-        
+
       ],
     },
   };
--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// middleware to use the client's dist folder
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// get the htmlRoutes.js file
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
